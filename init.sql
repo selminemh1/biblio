60936595112207c4650a9d75f34b5d44025d55a6
Selmine MH <selminemh@gmail.com>
2026-05-22 12:45:09 +0100

    dockerisation

---
+++ init.sql
@@ -0,0 +1,6 @@
+-- Script d'initialisation optionnel
+-- Hibernate crée les tables automatiquement (ddl-auto=update)
+-- Ce fichier peut servir pour insérer des données de démo si DataInitializer.java est désactivé
+
+-- Exemple : CREATE DATABASE IF NOT EXISTS librarydb;
+-- Les tables sont créées par Hibernate au premier démarrage
